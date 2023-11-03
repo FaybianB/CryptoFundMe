200a53f23f478a70891e425b527160822a2c2941
Faybian Byrd <faybian.byrd@gmail.com>
2023-11-03 00:58:00 -0500

    Update Solidity version in tests

--- backend/test/CryptoFundMe.t.sol
+++ backend/test/CryptoFundMe.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.22;
 
 import { Test } from "forge-std/Test.sol";
 import { CryptoFundMe, CampaignCreated, Donated, DeadlineChanged, TargetAmountChanged } from "../src/CryptoFundMe.sol";
